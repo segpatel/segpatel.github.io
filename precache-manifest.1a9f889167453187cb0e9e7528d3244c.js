f7a3f023a59f3b5f51398cbad3490f51142b9a82
Haresh V <haresh.v521@gmail.com>
2020-05-15 11:56:07 +0530

    all vue.js

---
+++ precache-manifest.1a9f889167453187cb0e9e7528d3244c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec113a191123ff60e12",
+    "url": "/css/app.ca936927.css"
+  },
+  {
+    "revision": "8660c75c53c5b9f5c252c26856d9a0b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a511896c12f723951510",
+    "url": "/js/about.4987f8c3.js"
+  },
+  {
+    "revision": "0ec113a191123ff60e12",
+    "url": "/js/app.cb772ab4.js"
+  },
+  {
+    "revision": "19e78f97c613afd61629",
+    "url": "/js/chunk-vendors.00092a4d.js"
+  },
+  {
+    "revision": "c7e49d5a6f9017f835dd3a2ec124a868",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
